cd1d3d49ecfdc4d5d28d0b1d6bb95fd3538561f3
MIKE <miguel.navarro@cua.uam.mx>
2025-06-22 19:47:23 -0600

    Actualizacion pass

--- sugerencias/src/main/java/mx/uam/ConexionMySQL.java
+++ sugerencias/src/main/java/mx/uam/ConexionMySQL.java
@@ -8,7 +8,7 @@ public class ConexionMySQL {
     // MALA PRACTICA : Hardconding !!!
     private static final String URL = "jdbc:mysql://localhost:3306/recomendador";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "Namm950924";
 
     private Connection conn;
 
